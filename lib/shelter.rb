f9e682fadc3315c03689bcf437deaf7a108ce3fd
Kaleb Kahssai <kalebkahssai@gmail.com>
2022-10-12 09:38:35 -0700

    will show completed first two iterations

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,29 @@
 class Shelter
+
+  def initialize(name, capacity)
+    @capacity = capacity
+    @name = name
+    @pets = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def name
+    @name
+  end
+
+  def pets
+    @pets
+  end
+
+  def add_pet(pet_name)
+    @pets << pet_name
+  end
+
+  def call_pets()
+    call_pets = ['Salem!', 'Beethoven!', 'Spot!', 'Jonesy!']
+  end
+
 end
